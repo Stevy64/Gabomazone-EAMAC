35d707aa348d019ba7bf7761a71a1aa797a4f472
Stevy64 <boubalastevy64@gmail.com>
2025-01-26 22:41:54 +0000

    PythonAnywhere: Working settings in prod

--- gabomazone-app/project/settings.py
+++ gabomazone-app/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mysecret'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gabomazone.pythonanywhere.com', '*']
 
 
 # Application definition
