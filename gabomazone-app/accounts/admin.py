8dace78208852bbcdbbdd2f530610c154f58fc7d
Stevy64 <boubalastevy64@gmail..com>
2025-12-02 11:31:26 +0100

    Admin Pannel - Design : OK

--- gabomazone-app/accounts/admin.py
+++ gabomazone-app/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile,BankAccount ,SocialLink
+from .models import Profile
 # Register your models here.
 
 class ProfileAdmin(admin.ModelAdmin):
@@ -12,31 +12,4 @@ class ProfileAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ("id", 'user__username',)
 
-
-
-class BankAccountAdmin(admin.ModelAdmin):
-    # inlines = [Inline_ProductImage, Inline_ProductAlternative]
-    fields = ("vendor_profile","bank_name", "account_number",  "swift_code",
-              "account_name", "country","paypal_email","description",)
-    list_display = ("id", "vendor_profile", "bank_name","account_number",
-                    "swift_code", "account_name","country","paypal_email",)
-    list_display_links = ("id", "bank_name", "paypal_email")
-
-    search_fields = ("account_name", )
-    list_per_page = 10
-
-class SocialLinkAdmin(admin.ModelAdmin):
-    # inlines = [Inline_ProductImage, Inline_ProductAlternative]
-    fields = ("vendor_profile","facebook", "twitter",  "instagram",
-              "pinterest",)
-    list_display = ("id", "vendor_profile", "facebook","twitter",
-                    "instagram", "pinterest",)
-    list_display_links = ("id", "vendor_profile", )
-    
-
-    search_fields = ("id", )
-    list_per_page = 10    
-
 admin.site.register(Profile,ProfileAdmin)
-admin.site.register(BankAccount,BankAccountAdmin)
-admin.site.register(SocialLink,SocialLinkAdmin)
