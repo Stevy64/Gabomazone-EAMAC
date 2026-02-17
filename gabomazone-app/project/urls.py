32992a89671f8e5cc8b5ebef1f12a3c57fefabe2
Stevy64 <boubalastevy64@gmail..com>
2026-02-17 11:53:18 +0100

    Patch - 'Reverse for faq not found' : OK

--- gabomazone-app/project/urls.py
+++ gabomazone-app/project/urls.py
@@ -19,12 +19,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from django.views.static import serve
+from pages.views import faq as faq_view
+
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('captcha/', include('captcha.urls')),
+    path('faq/', faq_view, name='faq'),
     path('', include('home.urls', namespace='home')),
     path('products/', include('products.urls', namespace='products')),
     path('', include('accounts.urls', namespace='accounts')),
